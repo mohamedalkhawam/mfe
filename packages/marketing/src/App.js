031f4b5db34d7d5da50a4580873a2607cbda3396
mohamed <mohamed.kh1994@gmail.com>
2023-05-08 12:18:44 +0300

    test prod

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Router } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
-
-export default function App() {
+export default function App({ history }) {
   return (
     <div>
       <StylesProvider>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route exact path="/pricing" component={Pricing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
